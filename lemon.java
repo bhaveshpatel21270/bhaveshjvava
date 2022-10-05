49307491b42fdca0ec39d2b779c8883d1489b5b3
marshearth07 <72681213+marshearth07@users.noreply.github.com>
2022-10-05 12:40:31 +0530

    Create lemon.java

---
+++ lemon.java
@@ -0,0 +1,8 @@
+        WebElement clickable = driver.findElement(By.id("clickable"));
+        new Actions(driver)
+                .moveToElement(clickable)
+                .pause(Duration.ofSeconds(1))
+                .clickAndHold()
+                .pause(Duration.ofSeconds(1))
+                .sendKeys("abc")
+                .perform();
